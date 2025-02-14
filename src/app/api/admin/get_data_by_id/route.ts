2173a78a5524834373655864d2ab3555ae1f3f58
Abdul (Career_Edwise) <careeredwise7@gmail.com>
2025-02-14 18:31:20 +0530

    fix TS

--- src/app/api/admin/get_data_by_id/route.ts
+++ src/app/api/admin/get_data_by_id/route.ts
@@ -1,6 +1,8 @@
 import { createSupabaseServerClient } from "@/lib/supabase"
 import { NextRequest, NextResponse } from "next/server"
 
+export const dynamic = "force-dynamic"
+
 export async function GET(request: NextRequest) {
   try {
     const { searchParams } = new URL(request.url)

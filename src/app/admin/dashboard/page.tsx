b84fc95141dba7048ff361c52949ebadd1a5d7f5
Abdul (Career_Edwise) <careeredwise7@gmail.com>
2025-02-15 15:22:19 +0530

    chart in progress

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import { BELayout } from "@/components/admin-panel/BELayout"
+import { Heading } from "@/components/admin-panel/Heading"
+import StackedAreaChart from "@/components/admin-panel/dashboard/ChartAreaStacked"
 
 export default function AdminDashboardPage() {
   return (
-    <BELayout>
-      <div>AdminDashboardPage</div>
+    <BELayout className="mb-10 tab:mb-0">
+      <StackedAreaChart />
     </BELayout>
   )
 }

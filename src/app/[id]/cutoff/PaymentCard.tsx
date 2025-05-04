10c9f171dff5ebced06bd46e3904e3ba51bb906a
Abdul (Career_Edwise) <careeredwise7@gmail.com>
2025-05-04 13:14:26 +0530

    test

--- src/app/[id]/cutoff/PaymentCard.tsx
+++ src/app/[id]/cutoff/PaymentCard.tsx
@@ -3,7 +3,7 @@ import { useInternalSearchParams } from "@/hooks/useInternalSearchParams"
 import { Shield } from "lucide-react"
 import React from "react"
 
-const PRICE = 49
+const PRICE = 1
 
 function PaymentCard({ showCutoff }: { showCutoff: () => void }) {
   const { getSearchParams } = useInternalSearchParams()

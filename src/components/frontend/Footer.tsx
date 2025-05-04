aa3439b5a0a2ce0dd64af3c80f50b4a9be04cd29
Abdul (Career_Edwise) <careeredwise7@gmail.com>
2025-05-04 13:05:54 +0530

    no change

--- src/components/frontend/Footer.tsx
+++ src/components/frontend/Footer.tsx
@@ -95,9 +95,12 @@ export function Footer() {
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                   />
                 </svg>
-                <span className="text-gray-400">
+                <a
+                  href="mailto:collegecutoff.net@gmail.com"
+                  className="text-gray-400"
+                >
                   collegecutoff.net@gmail.com
-                </span>
+                </a>
               </li>
               <li className="flex items-start gap-2">
                 <svg
@@ -113,7 +116,9 @@ export function Footer() {
                     d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                   />
                 </svg>
-                <span className="text-gray-400">+91 9522235235</span>
+                <a href="tel:+919028009835" className="text-gray-400">
+                  +91 9028009835
+                </a>
               </li>
             </ul>
           </div>

4acbe13b4250a8ba39266d9a229a860e0583a174
AbdulMoqueet <moqueetabdul@gmail.com>
2025-07-10 13:21:52 +0530

    remove type and lint checks

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  productionBrowserSourceMaps: false,
+}
 
 export default nextConfig
+
